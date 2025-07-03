5045a67d98726d724717e4cd79d1efa65f427178
Teike1 <random.mailw94u@gmail.com>
2025-07-03 13:17:03 +0200

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,9 +1,1 @@
-function login() {
-  const email = document.getElementById('email').value;
-  const password = document.getElementById('password').value;
-  firebase.auth().signInWithEmailAndPassword(email, password)
-    .then(() => {
-      document.getElementById('login-container').style.display = 'none';
-    })
-    .catch(error => alert("Fout bij inloggen: " + error.message));
-}
+
